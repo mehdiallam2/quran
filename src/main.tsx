4b1252e8bd0e1bcc3427a45d99050d87c5c43c94
Mahdi Allam <mahdiallam@Mahdis-MacBook-Pro.local>
2024-03-13 22:23:09 +0000

    add: deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "@styles/reset.css";
 import "@styles/fonts.css";
@@ -8,8 +8,8 @@ import "@styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
